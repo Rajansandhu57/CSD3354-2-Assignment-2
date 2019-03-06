4cd067934a53fe71d48b32dcb0dadeb112e8e4a7
visualstudio <visualstudio@122-PC30>
2019-03-06 12:16:05 -0500

    lab 6

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -22,32 +22,38 @@ namespace CSD3354_2_Assignment_2
                 DelegateExercises delegateExercises = new DelegateExercises();
                 delegateExercises.Method3();
                 Console.ReadLine();
-
             }
         }
 
-        public delegate void MyDelegate(ref int intValue);
-
         public class DelegateExercises
         {
-            void Method1(ref int intValue)
+            public delegate int MyDelegate(int intValue);
+
+            int Method1(int intMethod1)
             {
-                intValue = intValue + 5;
-                System.Console.WriteLine("Method1 " + intValue);
+                return intMethod1 * 2;
+            }
+
+            int Method2(int intMethod1)
+            {
+                return intMethod1 * 10;
+            }
+
+            public void Method4(MyDelegate myDelegate)
+            {
+                int result = myDelegate(10);
+                Console.WriteLine(result);
             }
 
             public void Method3()
             {
                 MyDelegate myDelegate = new MyDelegate(Method1);
-                MyDelegate myDelegate1 = new MyDelegate(Method1);
-                MyDelegate myDelegate2 = myDelegate + myDelegate1;
-                int intParameter = 5;
-                myDelegate2(ref intParameter);
+                Method4(myDelegate);
+                myDelegate = new MyDelegate(Method2);
+                Method4(myDelegate);
             }
         }
-
     }
 }
-
 
 
